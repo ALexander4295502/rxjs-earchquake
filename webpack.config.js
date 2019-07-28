db8dfd69a8886133af74944eabdc1f74138141f4
Zheng Yuan <yuanzheng@sportdex.io>
2019-07-27 19:53:49 -0700

    finish setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const glob = require("glob");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = env => ({
+  entry: glob.sync("./src/js/**/*.js"),
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  watch: env.dev,
+  devtool: env.dev ? "inline-source-map" : "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html/earthquake.html"
+    })
+  ]
+});
